94efedc5bda5e6102d06834265f3ec8f8717af78
MilaBullock <mdb6296@psu.edu>
2025-07-29 11:52:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,14 @@
-/*** You will not need this file until Unit 5 ***/
-/*** Dark Mode ***
-  
-  Purpose:
-  - Use this starter code to add a dark mode feature to your website.
-
-  When To Modify:
-  - [ ] Project 5 (REQUIRED FEATURE) 
-  - [ ] Any time after
-***/
-
-// Step 1: Select the theme button
 let themeButton = document.getElementById("theme-button");
 
 
-
-// Step 2: Write the callback function
 const toggleLightMode = () => {
     document.body.classList.toggle("light-mode");
-    // This section will run whenever the button is clicked
+   
 }
 
-// Step 3: Register a 'click' event listener for the theme button,
-//  and tell it to use toggleDarkMode as its callback function
 themeButton.addEventListener("click" , toggleLightMode);
 
 
-
-
-/*** Form Handling [PLACEHOLDER] [ADDED IN UNIT 6] ***/
-
-/*** Form Handling ***
-  
-  Purpose:
-  - When the user submits the RSVP form, the name and state they 
-    entered should be added to the list of participants.
-
-  When To Modify:
-  - [ ] Project 6 (REQUIRED FEATURE)
-  - [ ] Project 6 (STRETCH FEATURE) 
-  - [ ] Project 7 (REQUIRED FEATURE)
-  - [ ] Project 9 (REQUIRED FEATURE)
-  - [ ] Any time between / after
-***/
-
-// Step 1: Add your query for the submit RSVP button here
 const rsvpButton = document.getElementById("rsvp-button");
 
 const addParticipant = (person) => {
@@ -55,24 +20,7 @@ const addParticipant = (person) => {
   participantsDiv.appendChild(newParticipant);
 };
 
-// Step 3: Add a click event listener to the submit RSVP button here
 
-
-/*** Form Validation [PLACEHOLDER] [ADDED IN UNIT 7] ***/
-
-/*** Form Validation ***
-  
-  Purpose:
-  - Prevents invalid form submissions from being added to the list of participants.
-
-  When To Modify:
-  - [ ] Project 7 (REQUIRED FEATURE)
-  - [ ] Project 7 (STRETCH FEATURE)
-  - [ ] Project 9 (REQUIRED FEATURE)
-  - [ ] Any time between / after
-***/
-
-// Step 1: We actually don't need to select the form button again -- we already did it in the RSVP code above.
 const validateForm = () => {
   let containsErrors = false;
   const rsvpInputs = document.getElementById("rsvp-form").elements;
@@ -118,25 +66,11 @@ const validateForm = () => {
   }
 };
 
-// Step 3: Replace the form button's event listener with a new one that calls validateForm()
 rsvpButton.addEventListener("click", (event) => {
   event.preventDefault();  
   validateForm();          
 });
 
-/*** Animations [PLACEHOLDER] [ADDED IN UNIT 8] ***/
-/*** Success Modal [PLACEHOLDER] [ADDED IN UNIT 9] ***/
-
-/*** Modal ***
-  
-  Purpose:
-  - Use this starter code to add a pop-up modal to your website.
-
-  When To Modify:
-  - [ ] Project 9 (REQUIRED FEATURE)
-  - [ ] Project 9 (STRETCH FEATURE)
-  - [ ] Any time after
-***/
 
 const toggleModal = (person) => {
   const modal = document.getElementById("success-modal");
@@ -152,17 +86,7 @@ const toggleModal = (person) => {
   }, 5000);
 };
   
-  // TODO: Update modal display to flex
-  
 
-  // TODO: Update modal text to personalized message
-
-
-  // Set modal timeout to 5 seconds
-  
-
-
-// TODO: animation variables and animateImage() function
 let rotateFactor = 0;
 const modalImage = document.querySelector("#success-modal img");
 
